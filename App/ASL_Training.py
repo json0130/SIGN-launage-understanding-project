306b81dc0f4be7a9d8389f200ae78bfc1e630583
Joey Back <jbac208@aucklanduni.ac.nz>
2024-06-04 17:13:18 +1200

    training window shows plot

--- App/ASL_Training.py
+++ App/ASL_Training.py
@@ -5,23 +5,22 @@ from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as Navigatio
 import matplotlib.pyplot as plt
 
 class Ui_training_session(object):
-    # data should include model type, epochs, batch size
-    
     def setupUi(self, training_session, data):
         training_session.resize(600, 800)
         training_session.setMinimumSize(QtCore.QSize(600, 840))
         training_session.setStyleSheet("QMainWindow {background: #4d4d4d;}\n")
         self.centralwidget = QtWidgets.QWidget(training_session)
+        training_session.setCentralWidget(self.centralwidget)
 
         # Add a graph view
         self.view_graph = QVBoxLayout(self.centralwidget)
-        self.view_graph.setGeometry(QtCore.QRect(30, 20, 531, 540))
         
         # Create a figure and canvas
         self.figure = plt.figure()
         self.canvas = FigureCanvas(self.figure)
-        self.toolbar = NavigationToolbar(self.canvas, self.view_graph)
-
+        
+        # Create a toolbar and add it to the layout
+        self.toolbar = NavigationToolbar(self.canvas, training_session)
         self.view_graph.addWidget(self.toolbar)
         self.view_graph.addWidget(self.canvas)
 
@@ -42,7 +41,7 @@ class Ui_training_session(object):
         self.progressBar.setStyleSheet("QProgressBar {color: white;}")
         self.progressBar.setProperty("value", 24)
         # Add the progress bar to the grid layout
-        self.gridLayout.addWidget(self.progressBar_2, 1, 3, 1, 1)
+        self.gridLayout.addWidget(self.progressBar, 1, 3, 1, 1)
 
         # Create a Push Button
         self.pushButton_4 = QtWidgets.QPushButton(self.options_frame)
@@ -52,7 +51,7 @@ class Ui_training_session(object):
                                         "QPushButton:hover {background-color: #2A4BA0;}\n"
                                         "QPushButton:pressed {background-color: #1E3C8C;}")
         # Add the push button to the grid layout
-        self.gridLayout_5.addWidget(self.pushButton_4, 3, 3, 1, 1)
+        self.gridLayout.addWidget(self.pushButton_4, 3, 3, 1, 1)
         
         # Add a spacer items
         spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Minimum)
@@ -60,7 +59,7 @@ class Ui_training_session(object):
         spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Minimum)
         self.gridLayout.addItem(spacerItem1, 1, 4, 1, 1)
         spacerItem2 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
-        self.gridLayout_5.addItem(spacerItem2, 4, 3, 1, 1)
+        self.gridLayout.addItem(spacerItem2, 4, 3, 1, 1)
 
         # Create a label
         self.label = QtWidgets.QLabel(self.options_frame)
@@ -70,32 +69,30 @@ class Ui_training_session(object):
         spacerItem3 = QtWidgets.QSpacerItem(20, 20, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
         self.gridLayout.addItem(spacerItem3, 2, 3, 1, 1)
         
-        training_session.setCentralWidget(self.centralwidget)
-        self.statusbar = QtWidgets.QStatusBar(training_session)
-        training_session.setStatusBar(self.statusbar)
+        training_session.setStatusBar(QtWidgets.QStatusBar(training_session))
 
         self.retranslateUi(training_session)
         QtCore.QMetaObject.connectSlotsByName(training_session)
 
-def retranslateUi(self, training_session):
-	_translate = QtCore.QCoreApplication.translate
-	training_session.setWindowTitle(_translate("training_session", "TrainingSession"))
-	self.pushButton_4.setText(_translate("training_session", "Stop Training"))
-	self.label.setText(_translate("training_session", "                                         Training Progress"))
+    def retranslateUi(self, training_session):
+        _translate = QtCore.QCoreApplication.translate
+        training_session.setWindowTitle(_translate("training_session", "TrainingSession"))
+        self.pushButton_4.setText(_translate("training_session", "Stop Training"))
+        self.label.setText(_translate("training_session", "                                         Training Progress"))
 
-def plot(self, data1, data2):
-	# add code here to plot the data
-	self.canvas.draw()
+    def plot(self, data1, data2):
+        # add code here to plot the data
+        self.canvas.draw()
 
-def update_plot(self, data1, data2):
-	# add code here to update the plot with new data
-	self.plot(data1, data2)
+    def update_plot(self, data1, data2):
+        # add code here to update the plot with new data
+        self.plot(data1, data2)
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     training_session = QtWidgets.QMainWindow()
     ui = Ui_training_session()
-    ui.setupUi(training_session)
+    ui.setupUi(training_session, None)
     training_session.show()
     sys.exit(app.exec_())
