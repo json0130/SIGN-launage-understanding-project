a8da2422f3148506a797f0590b3008b0f165fd36
Joey Back <jbac208@aucklanduni.ac.nz>
2024-06-05 16:39:49 +1200

    hopefully working merged?

--- App/ASL_Trainer.py
+++ App/ASL_Trainer.py
@@ -330,7 +330,8 @@ class Ui_MainWindow(object):
 # =================================|| Functions ||=================================
     # Function to load model
     def loadModel(self):
-        self.model_file = QtWidgets.QFileDialog.getOpenFileNames(None, 'Open file', 'c:\\',"Image files (*.pth)")
+        model_file = QtWidgets.QFileDialog.getOpenFileNames(None, 'Open file', 'c:\\',"Model files (*.pth)")
+        self.model_file = model_file[0][0]
 
     def alphabetcheck(self, input):
         #checks if a string is a letter and if it is in the alphabet it converts it into a number from 0-25
@@ -591,7 +592,14 @@ class Ui_MainWindow(object):
 
     # Function that runs when the label is clicked
     def labelClicked(self, object_path):
+<<<<<<< Updated upstream
         results = resnet_app_test.predict_image(object_path)
+=======
+        print("clicked")
+        print(object_path)
+        print("clicked")
+        results = resnet_app_test.predict_image(object_path, self.model_file)
+>>>>>>> Stashed changes
 
         self.window = QtWidgets.QMainWindow()
         self.ui = Ui_Results()
